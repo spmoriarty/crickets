2bb011039e89504300e66f7fc43432d7ad3adafc
Brien Thomas <thomasbrien94@gmail.com>
2022-05-25 17:50:31 -0700

    displayMovies function works

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 import { getMovies } from './fetch-utils.js';
+import { renderMovies } from './render-util.js';
 
-//getMovies();
+const containerDisplay = document.querySelector('.container-display');
+
+async function displayMovies() {
+    const data = await getMovies();
+    for (let movie of data) {
+        const movieDiv = renderMovies(movie);
+        containerDisplay.append(movieDiv);
+    }
+}
+
+displayMovies();
--- render-util.js
+++ render-util.js
@@ -1,6 +1,4 @@
-// import { getMovies } from './fetch-utils.js';
-
-export function renderMovies() {
+export function renderMovies(movie) {
     const div = document.createElement('div');
     div.classList.add('movie-card');
 
@@ -8,10 +6,10 @@ export function renderMovies() {
     a.href = '#';
 
     const img = document.createElement('img');
-    img.src = './assets/The-Gentlemen.jpg';
+    img.src = `${movie.img}`;
 
     const h2 = document.createElement('h2');
-    h2.textContent = 'The Gentlemen';
+    h2.textContent = `${movie.title}`;
 
     const h3 = document.createElement('h3');
     h3.textContent = 'Rating: ';
