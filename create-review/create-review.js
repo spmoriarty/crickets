26731d59e4bc09e09e6af0e72242698602ec14df
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-26 17:30:22 -0700

    adds create-review page, styling and form styling

--- create-review/create-review.js
+++ create-review/create-review.js
@@ -1,7 +1,12 @@
 import { logout } from '../fetch-utils.js';
 
 const logoutButton = document.getElementById('auth-button');
+const homeButton = document.getElementById('home-button');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+homeButton.addEventListener('click', () => {
+    window.location.href = '/';
 });
